6e1ed725d19580510483fb9f8ab69ce8baa1c1bd
Momensamir12 <momenbitw5@gmail.com>
2025-07-26 16:58:44 +0300

    expiry

--- src/redis_command_handler/redis_command_handler.c
+++ src/redis_command_handler/redis_command_handler.c
@@ -43,7 +43,7 @@ char *handle_command(redis_db_t *db, char * buffer)
         char *args = parse_resp_array(resp_buffer);
         if (args) {
             response = encode_bulk_string(args);
-            free(args);  // Free the argument
+            free(args);
         }
     }
     else if (strcmp(command, "ping") == 0) {  
@@ -52,18 +52,46 @@ char *handle_command(redis_db_t *db, char * buffer)
     else if (strcmp(command, "set") == 0){
         char *key = parse_resp_array(resp_buffer);
         char *value = parse_resp_array(resp_buffer);
-        if(strcmp(parse_resp_array(resp_buffer), "px") == 0){
-          char * expiry = parse_resp_array(resp_buffer);
-          handle_set_command(db, key, value, expiry);
+        
+        if (!key || !value) {
+            response = encode_simple_string("ERR wrong number of arguments");
+        } else {
+            // Check if there are more arguments
+            char *next_arg = NULL;
+            if (len > 3) {  // SET key value [px time]
+                next_arg = parse_resp_array(resp_buffer);
+            }
+            
+            if (next_arg && strcasecmp(next_arg, "px") == 0) {
+                char *expiry = parse_resp_array(resp_buffer);
+                if (expiry) {
+                    handle_set_command(db, key, value, expiry);
+                    free(expiry);
+                } else {
+                    response = encode_simple_string("ERR syntax error");
+                }
+                free(next_arg);
+            } else {
+                handle_set_command(db, key, value, NULL);
+                if (next_arg) free(next_arg);
+            }
+            
+            if (!response) {
+                response = encode_simple_string("OK");
+            }
         }
-        else{
-        handle_set_command(db, key, value, NULL);
-        }
-        response = encode_simple_string("OK");
+        
+        if (key) free(key);
+        if (value) free(value);
     }
     else if (strcmp(command, "get") == 0){
-      char *key = parse_resp_array(resp_buffer);
-      response = handle_get_command(db, key);
+        char *key = parse_resp_array(resp_buffer);
+        if (key) {
+            response = handle_get_command(db, key);
+            free(key);
+        } else {
+            response = encode_simple_string("ERR wrong number of arguments");
+        }
     }
     else {
         // Unknown command
@@ -81,30 +109,35 @@ char *handle_command(redis_db_t *db, char * buffer)
 void handle_set_command(redis_db_t *db, char *key, char *value, char *expiry) {
     redis_object_t *obj = malloc(sizeof(redis_object_t));
     obj->type = REDIS_STRING;
-    obj->ptr = strdup(value);  
+    obj->ptr = strdup(value);
+    obj->expiry = 0;  
+    
     if(expiry != NULL){
-       set_expiry_ms(obj, atoi(expiry));
+        set_expiry_ms(obj, atoi(expiry));
     }
-    hash_table_set(db->dict, key, obj);
+    
+    hash_table_set(db->dict, strdup(key), obj); 
 }
 
 char* handle_get_command(redis_db_t *db, char *key) {
     void *value = hash_table_get(db->dict, key);
     if (!value) {
-        return NULL_RESP_VALUE;  
+        return strdup(NULL_RESP_VALUE);  
     }
     
     redis_object_t *obj = (redis_object_t *)value;
-    if(is_expired(obj))
-    {
-        return NULL_RESP_VALUE;
+    if(is_expired(obj)) {
+        // Remove expired key
+        hash_table_delete(db->dict, key);
+        redis_object_destroy(obj);
+        return strdup(NULL_RESP_VALUE);
     }
+    
     if (obj->type != REDIS_STRING) {
-        return "-WRONGTYPE Operation against a key holding the wrong kind of value\r\n";
+        return strdup("-WRONGTYPE Operation against a key holding the wrong kind of value\r\n");
     }
     
     char *str = (char *)obj->ptr;
-    
     char *response = encode_bulk_string(str);
     
     return response;
